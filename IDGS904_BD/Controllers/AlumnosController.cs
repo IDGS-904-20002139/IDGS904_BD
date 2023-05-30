bee58689cb44ddc8d486cfddfb5d35aaf4ad20ce
Paula Valencia <pau_doly@hotmail.com>
2023-05-30 17:51:20 -0600

    Proyecto base de datos

---
+++ IDGS904_BD/Controllers/AlumnosController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IDGS904_BD.Models;
+
+namespace IDGS904_BD.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            using(AlumnosDBContext dbAlumnos=new AlumnosDBContext())
+            {
+                //select * from alumnos
+                return View(dbAlumnos.Alumnos.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                return View();
+            }
+        }
+        public ActionResult Edit(int? id)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                Alumnos alumno = dbAlumnos.Alumnos.Find(id);
+                if (alumno == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(alumno);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Create(Alumnos alum)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                dbAlumnos.Alumnos.Add(alum);
+                dbAlumnos.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int? id)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                Alumnos alumno = dbAlumnos.Alumnos.Find(id);
+                if (alumno == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(alumno);
+            }
+        }
+        public ActionResult Edit(int? id, Alumnos alum)
+        {
+            using (AlumnosDBContext dbAlumnos = new AlumnosDBContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    dbAlumnos.Entry(alum).State = EntityState.Modified;
+                    dbAlumnos.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(alum);
+            }
+        }
+    }
+}
---
+++ IDGS904_BD/Models/Alumnos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IDGS904_BD.Models
+{
+    public class Alumnos
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public String Nombre { get; set; }
+        [Required]
+        [StringLength(100)]
+        public String Apellidos { get; set; }
+        [StringLength(50)]
+        public String Correo { get; set; }
+    }
+}
